0da8841cadf978869fb9b722b03d7c27bd872ce3
m020202 <m020202@naver.com>
2024-09-04 15:51:29 +0900

    feat: payment 기능 구현

---
+++ src/main/java/spring/HelloSpring/Payment.java
@@ -0,0 +1,39 @@
+package spring.HelloSpring;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Getter
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    // 환율
+    private BigDecimal exRate;
+    // 환산된 원화 금액
+    private BigDecimal convertAmount;
+    // 환산 금액 유효 기간
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertAmount = convertAmount;
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertAmount=" + convertAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
