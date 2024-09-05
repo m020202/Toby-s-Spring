6b492df58d2026915f763bd59d68099e94f7d998
m020202 <m020202@naver.com>
2024-09-05 19:14:40 +0900

    feat: 오브젝트 팩토리 구현

---
+++ src/main/java/spring/HelloSpring/ObjectFactory.java
@@ -0,0 +1,11 @@
+package spring.HelloSpring;
+
+public class ObjectFactory {
+    public  PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
