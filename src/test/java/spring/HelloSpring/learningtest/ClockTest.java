72ab79f30b170c8515dd8ef5928902889f4fcdb3
m020202 <m020202@naver.com>
2024-09-12 19:53:06 +0900

    feat: Clock 학습 테스트 구현

---
+++ src/test/java/spring/HelloSpring/learningtest/ClockTest.java
@@ -0,0 +1,33 @@
+package spring.HelloSpring.learningtest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class ClockTest {
+    // Clock 을 이용해서 LocalDateTime now ?
+    @Test
+    void clock() {
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        Assertions.assertThat(dt2).isAfter(dt1);
+    }
+
+    // Clock 을 테스트에서 사용 시 원하는 시간을 지정해서 현재 시간으로 가져올 수 있는가?
+    @Test
+    void fixedClock() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        Assertions.assertThat(dt1).isEqualTo(dt2);
+    }
+}
