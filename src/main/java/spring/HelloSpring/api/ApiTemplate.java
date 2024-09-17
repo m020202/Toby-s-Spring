05901cb3e48fdf078c86bce3a92505e4c55feb82
m020202 <m020202@naver.com>
2024-09-17 12:00:42 +0900

    feat: ApiTemplate 클래스 구현으로 템플릿 분리

---
+++ src/main/java/spring/HelloSpring/api/ApiTemplate.java
@@ -0,0 +1,33 @@
+package spring.HelloSpring.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ApiTemplate {
+    public static BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        // URL을 준비하고 예외처리를 위한 작업
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            response = apiExecutor.execute(uri);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return exRateExtractor.extract(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/spring/HelloSpring/exrate/WebApiExRateProvider.java
+++ src/main/java/spring/HelloSpring/exrate/WebApiExRateProvider.java
@@ -1,45 +1,17 @@
 package spring.HelloSpring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import spring.HelloSpring.api.ApiExecutor;
-import spring.HelloSpring.api.ErApiExRateExtractor;
-import spring.HelloSpring.api.ExRateExtractor;
-import spring.HelloSpring.api.SimpleApiExecutor;
+import spring.HelloSpring.api.*;
 import spring.HelloSpring.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
-    }
-
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        // URL을 준비하고 예외처리를 위한 작업
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
     }
 }
