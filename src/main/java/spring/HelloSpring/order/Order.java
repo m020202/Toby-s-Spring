452247591a3fd7f47be5f2b38f684bbc7738b556
m020202 <m020202@naver.com>
2024-09-19 17:12:52 +0900

    feat: Order 제약 조건 추가

--- src/main/java/spring/HelloSpring/order/Order.java
+++ src/main/java/spring/HelloSpring/order/Order.java
@@ -1,9 +1,16 @@
 package spring.HelloSpring.order;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue
     private Long id;
+    @Column(unique = true)
     private String no;
     private BigDecimal total;
 }
