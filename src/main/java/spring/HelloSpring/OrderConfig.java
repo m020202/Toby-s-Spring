12ff0eb903f01d236c7191c7bd6170ffc22dabe4
m020202 <m020202@naver.com>
2024-09-26 23:55:01 +0900

    chore: OrderConfig 수정

--- src/main/java/spring/HelloSpring/OrderConfig.java
+++ src/main/java/spring/HelloSpring/OrderConfig.java
@@ -1,6 +1,5 @@
 package spring.HelloSpring;
 
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
