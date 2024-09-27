7ce71cd58913dd19f61cf9583a599a74686f44de
m020202 <m020202@naver.com>
2024-09-27 15:42:01 +0900

    feat: OrderService order 객체 생성 테스트 구현

--- src/test/java/spring/HelloSpring/order/OrderServiceSpringTest.java
+++ src/test/java/spring/HelloSpring/order/OrderServiceSpringTest.java
@@ -1,5 +1,6 @@
 package spring.HelloSpring.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,19 @@ import spring.HelloSpring.OrderConfig;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = OrderConfig.class)
 public class OrderServiceSpringTest {
     @Autowired // spring extension에 의해서 컨테이너 초기화 후 자동으로 빈 주입
     OrderService orderService;
 
+    @Test
+    void createOrder() {
+        var order = orderService.createOrder("0100", BigDecimal.TEN);
+
+        assertThat(order.getId()).isGreaterThan(0);
+    }
 
 }
